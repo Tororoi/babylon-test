79c0656e5202df3682a929b8fb76abc911f711da
Tororoi <cantwell.tom@gmail.com>
2020-11-18 20:15:24 -0500

    added container

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-window.addEventListener("DOMContentLoaded", function() {
-    let canvas = document.getElementById("canvas");
-    let engine = new BABYLON.Engine(canvas,true);
 
-    let createScene = function() {
-        let scene = new BABYLON.Scene(engine);
-        scene.clearColor = new BABYLON.Color3.White();
-        let camera = new BABYLON.FreeCamera("camera1",
-            new BABYLON.Vector3(0,6,-10),scene);
-        camera.setTarget(BABYLON.Vector3.Zero());
-        let box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
-        return scene;
-    };
+let canvas = document.getElementById("canvas");
+let engine = new BABYLON.Engine(canvas,true);
 
-    let scene = createScene();
-    engine.runRenderLoop(function() {
-        scene.render();
-    });
-})
+let createScene = function() {
+    let scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3.White();
+    let camera = new BABYLON.FreeCamera("camera1",
+        new BABYLON.Vector3(0,6,-10),scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+    let box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
+    return scene;
+};
+
+let scene = createScene();
+engine.runRenderLoop(function() {
+    scene.render();
+});
